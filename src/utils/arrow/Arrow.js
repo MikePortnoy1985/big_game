7ac7e00544917589f8ca69e0ec490b6d0b3267c9
MikePortnoy1985 <m.temartsev@gmail.com>
2021-02-10 03:03:41 +0300

    Homework#7

---
+++ src/utils/arrow/Arrow.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import s from './Arrow.module.css'
+import { useHistory } from 'react-router-dom'
+
+export const Arrow = () => {
+   const history = useHistory()
+
+   const handleGoHome = () => {
+      history.push('/')
+   }
+
+   return (
+      <div className={s.root} onClick={e => e && handleGoHome()}>
+         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/anchor-has-content */}
+         <a href='#' className={s.arrow} />
+      </div>
+   )
+}
+
+Arrow.propTypes = {
+   text: PropTypes.string.isRequired,
+}
